80f73aad436a86c2c65b0c8af0a64fb11194df06
Leo17220310 <125191929+Leo17220310@users.noreply.github.com>
2024-07-15 13:04:41 +0200

    new commit

--- src/sections/inputField.tsx
+++ src/sections/inputField.tsx
@@ -48,7 +48,7 @@ export function InputWithButton({ addCityData, cityCount }: InputWithButtonProps
     <div className="flex w-full max-w-sm items-center space-x-2">
       <Input
         type="text"
-        placeholder="Stadt / Land / Bezirk . . ."
+        placeholder="Stadt / Bezirk . . ."
         value={city}
         onChange={(e) => setCity(e.target.value)}
       />
@@ -58,5 +58,9 @@ export function InputWithButton({ addCityData, cityCount }: InputWithButtonProps
     </div>
   );
 }
+
+
+
+
 
 export default InputWithButton;
